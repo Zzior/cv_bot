c93c76f42d3181c3ecd7d293f8f70d166d9b5700
Zzior <xasan10102020@gmail.com>
2025-10-30 10:48:05 +0500

    ⚙️ Add Camera get_fps

--- src/services/camera.py
+++ src/services/camera.py
@@ -1,3 +1,4 @@
+import time
 import asyncio
 from urllib.parse import urlparse
 
@@ -45,15 +46,55 @@ class Camera:
         import cv2
 
         capture = cv2.VideoCapture(self.source, cv2.CAP_FFMPEG)
+        capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         if not capture.isOpened():
             return None
 
-        status, frame = capture.read()
-        if not status:
+        try:
+            status, frame = capture.read()
+            if not status:
+                return None
+
+            status, jpeg = cv2.imencode(".jpg", frame)
+            if not status:
+                return None
+
+            return jpeg.tobytes()
+
+        finally:
+            capture.release()
+
+    def get_fps(self, calc_frames: int = 30, drop_frames: int = 15, attempts: int = 5) -> float | None:
+        import cv2
+
+        capture = cv2.VideoCapture(self.source, cv2.CAP_FFMPEG)
+        capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        if not capture.isOpened():
             return None
 
-        status, jpeg = cv2.imencode(".jpg", frame)
-        if not status:
-            return None
+        dropped = 0
+        frames_ts = []
+        drop_attempts = 0
+        calc_attempts = 0
 
-        return jpeg.tobytes()
+        try:
+            while dropped < drop_frames and drop_attempts < attempts:
+                status = capture.grab()
+                if not status:
+                    drop_attempts += 1
+                    continue
+                dropped += 1
+
+            while len(frames_ts) < calc_frames and calc_attempts < attempts:
+                status, _ = capture.read()
+                if not status:
+                    calc_attempts += 1
+                    continue
+                frames_ts.append(time.perf_counter())
+
+            if len(frames_ts) > 1:
+                return (len(frames_ts) - 1) / (frames_ts[-1] - frames_ts[0])
+            else:
+                return None
+        finally:
+            capture.release()
