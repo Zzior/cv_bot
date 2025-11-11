972f3d78bfc4989fffdc6d579116195135cdf020
Zzior <xasan10102020@gmail.com>
2025-11-11 16:17:07 +0500

    🎯 Add ROI for video_reader

--- src/services/base/video_reader/conf.py
+++ src/services/base/video_reader/conf.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class VideoReaderConf(BaseModel):
     model_config = ConfigDict(extra="forbid", frozen=True)
 
     source: str
+
+    roi: list[list[int]] = Field(default_factory=list)
--- src/services/base/video_reader/node.py
+++ src/services/base/video_reader/node.py
@@ -1,18 +1,36 @@
 from typing import Generator
 
 import cv2
+import numpy as np
 
 from .conf import VideoReaderConf
-
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    import numpy as np
 
 
 class VideoReader:
     def __init__(self, params: VideoReaderConf) -> None:
         self.params = params
         self.capture = None
+
+        if params.roi:
+            self.cut_xyxy, self.roi_mask = self.calc_roi(params.roi)
+        else:
+            self.cut_xyxy, self.roi_mask = None, None
+
+    @staticmethod
+    def calc_roi(roi) -> tuple[tuple[int, int, int, int], np.ndarray]:
+        x = [i[0] for i in roi]
+        y = [i[1] for i in roi]
+
+        cut_xyxy = (min(x), min(y), max(x), max(y))
+
+        out_roi = [[i[0] - cut_xyxy[0], i[1] - cut_xyxy[1]] for i in roi]
+        out_roi = np.array(out_roi, dtype=np.int32)
+
+        roi_mask = np.zeros([cut_xyxy[3] - cut_xyxy[1], cut_xyxy[2] - cut_xyxy[0]], dtype=np.uint8)
+        roi_mask = cv2.fillPoly(roi_mask, [out_roi], (255, 255, 255))
+
+        return cut_xyxy, roi_mask
+
 
     def _connect_to_stream(self) -> bool:
         if isinstance(self.capture, cv2.VideoCapture):
@@ -26,7 +44,7 @@ class VideoReader:
 
         return False
 
-    def process(self) -> Generator["np.ndarray", None, None]:
+    def process(self) -> Generator[np.ndarray, None, None]:
         self._connect_to_stream()
 
         try:
@@ -39,6 +57,13 @@ class VideoReader:
 
                 if frame is None:
                     continue
+
+                if self.params.roi:
+                    frame = frame[
+                        self.cut_xyxy[1]:self.cut_xyxy[3],
+                        self.cut_xyxy[0]:self.cut_xyxy[2]
+                    ]
+                    frame = cv2.bitwise_and(frame, frame, mask=self.roi_mask)
 
                 yield frame
 
