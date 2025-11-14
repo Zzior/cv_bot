d2405d4548080e8bb97cb97196238ff8a90a2dc9
Zzior <xasan10102020@gmail.com>
2025-11-14 14:52:58 +0500

    🧩 Add DatasetCollector using parameters

--- src/bot/routers/dataset.py
+++ src/bot/routers/dataset.py
@@ -7,7 +7,7 @@ from aiogram.fsm.context import FSMContext
 from ..states import BotState
 from ..parsers import parse_date
 from ..keyboards import back_rkb, task_rkb, now_rkb, confirm_params_rkb
-from ..navigation import to_main_menu, to_datasets, choose_camera, choose_weights
+from ..navigation import to_main_menu, to_datasets, choose_camera, to_params
 
 from services.base.detection.conf import DetectionConf
 from services.base.video_reader.conf import VideoReaderConf
@@ -117,29 +117,64 @@ async def datasets_enter_end_handler(message: Message, state: FSMContext, t: Tra
         await message.answer("❗️" + t("enter_end_time", lang))
 
 
+async def get_reader(data: dict, app: App) -> VideoReaderConf:
+    async with app.db.session() as db:
+        camera = await db.camera.get_by_name(data["camera_name"])
+        camera_source = camera.source
+        camera_roi = camera.roi
+
+    return VideoReaderConf(
+        source=camera_source,
+        roi=camera_roi if data["use_roi"] else [],
+        skip_frames=data["skip_frames"],
+    )
+
+async def get_detector(data: dict, app: App) -> DetectionConf | None:
+    if data["weights_name"] and ["cls_conf"]:
+        async with app.db.session() as db:
+            weights = await db.weight.get_by_name(data["weights_name"])
+            weights_path = weights.path
+
+        minimal_conf = 1.0
+        classes = []
+        for cls, val in data["cls_conf"].items():
+            classes.append(cls)
+
+            if val[0] < minimal_conf:
+                minimal_conf = val[0]
+
+        return DetectionConf(
+            weights_path=weights_path,
+            classes=classes,
+            confidence=minimal_conf,
+            iou=data.get("iou") or DetectionConf.model_fields["iou"].default
+        )
+    else:
+        return None
+
 @dataset_router.message(BotState.datasets_confirm_params)
 async def datasets_confirm_params_handler(message: Message, state: FSMContext, t: Translator, lang: str, app: App) -> None:
     if message.text == t("b.back", lang):
         await state.set_state(BotState.datasets_enter_end)
         await message.answer(t("enter_end_time", lang), reply_markup=back_rkb(t, lang))
 
+    elif message.text == t("b.parameters", lang):
+        await state.set_state(BotState.params)
+        await to_params(
+            message, state, t, lang,
+            BotState.datasets_confirm_params,
+            access_params=["b.skip_frames", "b.use_roi", "b.weights", "b.cls_conf", "b.iou", "b.ignore_zone"]
+        )
+
     elif message.text == t("b.confirm", lang):
         data = await state.get_data()
-        async with app.db.session() as db:
-            camera = await db.camera.get_by_name(data["camera_name"])
-            camera_source = camera.source
-            camera_roi = camera.roi
 
         start = datetime.fromisoformat(data["start_date"])
         end = datetime.fromisoformat(data["end_date"])
         dir_name = start.strftime("%Y%m%d_%H%M%S")
 
-        reader = VideoReaderConf(
-            source=camera_source,
-            roi=camera_roi if data["use_roi"] else [],
-            skip_frames=data["skip_frames"],
-        )
-        detection = None
+        reader = await get_reader(data, app)
+        detection = await get_detector(data, app)
 
         await app.task_manager.add_task(
             start=start,
@@ -148,7 +183,8 @@ async def datasets_confirm_params_handler(message: Message, state: FSMContext, t
                 save_dir=str(app.config.storage_dir / "Datasets" / dir_name),
                 reader=reader,
                 detection=detection,
-
+                classes_conf=data.get("cls_conf") or {},
+                ignore_zone_ratio=data.get("ignore_zone_ratio") or DatasetCollectorConf.model_fields["ignore_zone_ratio"].default,
             )
         )
         await message.answer(t("task.created", lang))
